9de9a58c285329e734e3d1c2857fed9cdcd550fc
Askar Karasaev <askar.karasaev@gmail.com>
2020-08-07 13:43:22 +0900

    Svelte Router Setup

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,25 @@
 <script>
-	//pages
-	import Login from './pages/Login.svelte'
-	import Checkout from './pages/Checkout.svelte'
-	import ProductTemplate from './pages/ProductTemplate.svelte'
-	import Products from './pages/Products.svelte'
-	import Home from './pages/Home.svelte'
-	import About from './pages/About.svelte'
+  //pages
+  import Login from "./pages/Login.svelte";
+  import Checkout from "./pages/Checkout.svelte";
+  import ProductTemplate from "./pages/ProductTemplate.svelte";
+  import Products from "./pages/Products.svelte";
+  import Home from "./pages/Home.svelte";
+  import About from "./pages/About.svelte";
+  // router
+  import { Router, Route, Link } from "svelte-routing";
+  import { component_subscribe } from "svelte/internal";
 </script>
 
-<h1>hello</h1>
+<Router>
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="/about">About</Link>
+    <Link to="/products">Products</Link>
+  </nav>
+  <Route path="/" component={Home} />
+  <Route path="/about" component={About} />
+  <Route path="/login" component={Login} />
+  <Route path="/products" component={Products} />
+  <Route path="/products/:id" component={ProductTemplate} />
+</Router>
