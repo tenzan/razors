3d4b8fcec75ca86c824aeb1baf82b28b98e1e05d
Askar Karasaev <askar.karasaev@gmail.com>
2020-08-09 14:42:32 +0900

    Products Componenet Complete

--- src/components/Products/Products.svelte
+++ src/components/Products/Products.svelte
@@ -1,19 +1,29 @@
 <script>
+  export let title = "";
   import { onMount, onDestroy } from "svelte";
   import products from "../../stores/defaultProducts";
+  import Product from "./Product.svelte";
 
-//   onMount(() => {
-//       unsubscribe = products.subscribe(value => {
-//           console.log("subscribed");
-//           localProducts = value;
-//       });
-//   });
-//   onDestroy(()=>{
-//       unsubscribe();
-//   })
+  //   onMount(() => {
+  //       unsubscribe = products.subscribe(value => {
+  //           console.log("subscribed");
+  //           localProducts = value;
+  //       });
+  //   });
+  //   onDestroy(()=>{
+  //       unsubscribe();
+  //   })
 </script>
 
-<h1>hello from products component</h1>
-{#each $products as product}
-    <h2>{product.title}</h2>
-{/each}
+<section class="section">
+  <h2 class="section-title">{title}</h2>
+
+  <div class="products-center">
+
+    {#each $products as product (product.id)}
+      <Product {product} />
+    {/each}
+
+  </div>
+
+</section>
