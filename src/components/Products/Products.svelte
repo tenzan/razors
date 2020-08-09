0938d12d33c180fcbc0f504870c5c2ceab41d389
Askar Karasaev <askar.karasaev@gmail.com>
2020-08-09 14:26:36 +0900

    Store Unsubscribe

---
+++ src/components/Products/Products.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { onMount, onDestroy } from "svelte";
+  import products from "../../stores/defaultProducts";
+  let localProducts = [];
+  let unsubscribe;
+  onMount(() => {
+      unsubscribe = products.subscribe(value => {
+          console.log("subscribed");
+          localProducts = value;
+      });
+  });
+  onDestroy(()=>{
+      unsubscribe();
+  })
+</script>
+
+<h1>hello from products component</h1>
+{#each localProducts as product}
+    <h2>{product.title}</h2>
+{/each}
